b4e86459dd23842e17bac93a1bc0a599bb232541
Yong-Soo Chung <yongsoochung@gmail.com>
2014-10-27 15:34:58 -0700

    [TASK] Login: Fix wallet path issue

--- src/js/entry/desktop.js
+++ src/js/entry/desktop.js
@@ -34,6 +34,7 @@ require('../services/popup');
 require('../services/rippletxt');
 require('../services/federation');
 require('../services/domainalias');
+require('../services/filedialog');
 
 require('../services/integration/appManager');
 require('../services/integration/profileManager');
@@ -53,6 +54,7 @@ var appDependencies = [
   'id',
   'tracker',
   'appManager',
+  'filedialog',
   // Directives
   'charts',
   'effects',
