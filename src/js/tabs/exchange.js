f8a03881e9ec944b4e99d154506770ecc0384bb1
MatthewPhinney <matt.phinney@ripple.com>
2015-10-01 10:51:53 -0700

    [TASK] Comply with style guidlines

--- src/js/tabs/exchange.js
+++ src/js/tabs/exchange.js
@@ -26,6 +26,37 @@ ExchangeTab.prototype.angular = function (module)
     'rpId', 'rpNetwork', 'rpKeychain', '$rootScope',
     function ($scope, $timeout, $routeParams, $id, $network, keychain, $rootScope)
     {
+
+      function setEngineStatus(res, accepted) {
+        $scope.engine_result = res.engine_result;
+        $scope.engine_result_message = res.engine_result_message;
+        switch (res.engine_result.slice(0, 3)) {
+        case 'tes':
+          $scope.tx_result = accepted ? 'cleared' : 'pending';
+          break;
+        case 'tem':
+          $scope.tx_result = 'malformed';
+          break;
+        case 'ter':
+          $scope.tx_result = 'failed';
+          break;
+        case 'tep':
+          $scope.tx_result = 'partial';
+          break;
+        case 'tec':
+          $scope.tx_result = 'claim';
+          break;
+        case 'tef':
+          $scope.tx_result = 'failure';
+          break;
+        case 'tel':
+          $scope.tx_result = 'local';
+          break;
+        default:
+          console.warn('Unhandled engine status encountered!');
+        }
+      }
+
       if (!$id.loginStatus) $id.goId();
 
       var timer, pf;
@@ -121,7 +152,9 @@ ExchangeTab.prototype.angular = function (module)
           $scope.exchange.path_status = 'pending';
           var amount = $scope.exchange.amount_feedback;
 
-          if (amount.is_zero()) return;
+          if (amount.is_zero()) {
+            return;
+          }
 
           // Start path find
           $network.remote.createPathFind({
@@ -143,39 +176,39 @@ ExchangeTab.prototype.angular = function (module)
                 lastUpdate = new Date();
 
                 clearInterval(timer);
-                timer = setInterval(function(){
-                  $scope.$apply(function(){
-                    var seconds = Math.round((new Date() - lastUpdate)/1000);
+                timer = setInterval(function() {
+                  $scope.$apply(function() {
+                    var seconds = Math.round((new Date() - lastUpdate) / 1000);
                     $scope.lastUpdate = seconds ? seconds : 0;
                   })
                 }, 1000);
 
                 if (!upd.alternatives || !upd.alternatives.length) {
-                  $scope.exchange.path_status  = "no-path";
+                  $scope.exchange.path_status = 'no-path';
                   $scope.exchange.alternatives = [];
                 } else {
                   var currencies = {};
-                  $scope.exchange.path_status  = "done";
+                  $scope.exchange.path_status = 'done';
                   $scope.exchange.alternatives = _.filter(_.map(upd.alternatives, function (raw) {
                     var alt = {};
 
-                    alt.amount   = Amount.from_json(raw.source_amount);
+                    alt.amount = Amount.from_json(raw.source_amount);
 
                     // Waiting on response from ripple-lib team to fix rate
                     //alt.rate     = alt.amount.ratio_human(amount);
 
                     // Send max is 1.01 * amount (scaling amount is in integer drops)
                     alt.send_max = alt.amount.scale(101000000000000);
-                    alt.paths    = raw.paths_computed
+                    alt.paths = raw.paths_computed
                         ? raw.paths_computed
                         : raw.paths_canonical;
 
-                    if (alt.amount.issuer().to_json() != $scope.address && !isIssuer[alt.amount.currency().to_hex()]) {
-                      currencies[alt.amount.currency().to_hex()] = true
+                    if (alt.amount.issuer().to_json() !== $scope.address && !isIssuer[alt.amount.currency().to_hex()]) {
+                      currencies[alt.amount.currency().to_hex()] = true;
                     }
 
                     return alt;
-                  }), function(alt) {
+                  }), function() {
                     return true;
                   });
                 }
@@ -264,7 +297,7 @@ ExchangeTab.prototype.angular = function (module)
         // parse the currency name and extract the iso
         var currency = Currency.from_human($scope.exchange.currency_name);
         currency = currency.has_interest() ? currency.to_hex() : currency.get_iso();
-        var amount = Amount.from_human(""+$scope.exchange.amount+" "+currency);
+        var amount = Amount.from_human('' + $scope.exchange.amount + ' ' + currency);
 
         amount.set_issuer($id.account);
 
@@ -289,10 +322,10 @@ ExchangeTab.prototype.angular = function (module)
           keychain.requestSecret($id.account, $id.username,
             function (err, secret) {
               if (err) {
-                console.log("client: exchange tab: error while " +
-                  "unlocking wallet: ", err);
-                $scope.mode = "error";
-                $scope.error_type = "unlockFailed";
+                console.log('client: exchange tab: error while ' +
+                  'unlocking wallet: ', err);
+                $scope.mode = 'error';
+                $scope.error_type = 'unlockFailed';
 
                 return;
               }
@@ -309,40 +342,38 @@ ExchangeTab.prototype.angular = function (module)
             $scope.exchanged(tx.hash);
 
             // Remember currency and increase order
-            var found;
 
             for (var i = 0; i < $scope.currencies_all.length; i++) {
               if ($scope.currencies_all[i].value.toLowerCase() === $scope.exchange.amount_feedback.currency().get_iso().toLowerCase()) {
                 $scope.currencies_all[i].order++;
-                found = true;
                 break;
               }
             }
           });
         });
-        tx.on('success',function(res){
+        tx.on('success', function(res) {
           setEngineStatus(res, true);
         });
         tx.on('error', function (res) {
           setImmediate(function () {
             $scope.$apply(function () {
-              $scope.mode = "error";
+              $scope.mode = 'error';
 
-              if (res.result === "tejMaxFeeExceeded") {
-                $scope.error_type = "maxFeeExceeded";
+              if (res.result === 'tejMaxFeeExceeded') {
+                $scope.error_type = 'maxFeeExceeded';
               }
 
               if (res.error === 'remoteError' &&
                   res.remote.error === 'noPath') {
-                $scope.mode = "status";
-                $scope.tx_result = "noPath";
+                $scope.mode = 'status';
+                $scope.tx_result = 'noPath';
               }
             });
           });
         });
         tx.submit();
 
-        $scope.mode = "sending";
+        $scope.mode = 'sending';
       };
 
       /**
@@ -361,36 +392,6 @@ ExchangeTab.prototype.angular = function (module)
           });
         }
       };
-
-      function setEngineStatus(res, accepted) {
-        $scope.engine_result = res.engine_result;
-        $scope.engine_result_message = res.engine_result_message;
-        switch (res.engine_result.slice(0, 3)) {
-          case 'tes':
-            $scope.tx_result = accepted ? "cleared" : "pending";
-            break;
-          case 'tem':
-            $scope.tx_result = "malformed";
-            break;
-          case 'ter':
-            $scope.tx_result = "failed";
-            break;
-          case 'tep':
-            $scope.tx_result = "partial";
-            break;
-          case 'tec':
-            $scope.tx_result = "claim";
-            break;
-          case 'tef':
-            $scope.tx_result = "failure";
-            break;
-          case 'tel':
-            $scope.tx_result = "local";
-            break;
-          default:
-            console.warn("Unhandled engine status encountered!");
-        }
-      }
 
       $scope.reset();
 
